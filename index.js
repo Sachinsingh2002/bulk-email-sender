267e55f1157899218a5274c34c4749e4a70cfd18
sachinsingh2002 <sachinsingh2002.ssss@gmail.com>
2024-12-27 15:20:22 +0530

    *changes in the index.js file..

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ const newTransporter = ()=>{
     auth: {
 
       user: "sachinsingh2002.ssss@gmail.com",
+      pass: "pass pass pass" // add your app password here
+    },
+  });
+}
+const transporter = newTransporter();
+const sendEmail = async (row) => {
+  const { Name, Company, Email, Role, Link } = row; // Adjust column names accordingly
+  const nameParts = Name.split(' ');
+  const name = nameParts[0];
+  const mailOptions = {
+    from: 'Sachin Singh <sachinsingh2002.ssss@gmail.com>',
+    to: Email,
+    subject: `Request for an Interview Opportunity - ${Role} at ${Company}`,
+    html: `
