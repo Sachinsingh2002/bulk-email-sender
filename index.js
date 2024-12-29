bf4b3aea548aa1baf9a1dab8e5c6c79754d05202
sachinsingh2002 <sachinsingh2002.ssss@gmail.com>
2024-12-29 14:15:58 +0530

    *updated index.js file..

--- index.js
+++ index.js
@@ -42,3 +42,15 @@ const sendEmail = async (row) => {
       I got to know through your LinkedIn post that <b>${Company}</b> is looking for a <b>${Role}</b>, 
       therefore, I have mailed you to tell you about myself. I have:
     </p>
+    <ul>
+      <li><b>Experience:</b> Web Developer at <a href="https://www.rnpsoft.com/">RnPsoft Pvt. Ltd.</a>, contributing to 
+      responsive, cross-browser-compatible web solutions.</li>
+      <li><b>Data Analyst:</b> Worked at <a href="https://www.galleonconsultants.com/">Galleon Consultants</a>, building 
+      10+ dashboards using <b>Power BI, Tableau, and Excel</b>, improving decision-making by 30%.</li>
+      <li>Optimized the order system for <b>Healermate</b>, improving accuracy by 15% using WordPress.</li>
+      <li>Worked extensively with <b>JavaScript, React, Next.js, Firebase, Supabase</b>, and more.</li>
+      <li>Developed <b>PoemGPT</b>, an AI-driven web app using Next.js, Tailwind CSS, and Replicate API.</li>
+      <li>Created a <b>Spam Detection Web App</b> using Python and Flask with 92% accuracy.</li>
+      <li>Bachelor's in Computer Science and Engineering from <b>Galgotias University, 2025 Grad</b>.</li>
+      <li>Proficient in tools like <b>Power BI, GitHub, and Tableau</b>.</li>
+    </ul>
